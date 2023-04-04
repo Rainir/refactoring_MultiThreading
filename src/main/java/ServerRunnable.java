66b4ddcf8dcfd56dc89d53fafc2e7e4534a01782
Rainir <off_rainir@proton.me>
2023-04-04 17:25:39 +0300

    Fixes

---
+++ src/main/java/ServerRunnable.java
@@ -0,0 +1,69 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ServerRunnable extends Thread {
+    public static Runnable setOut(BufferedReader in, BufferedOutputStream out, List<String> validPaths) {
+        try {
+            while (true) {
+                final var requestLine = in.readLine();
+                final var parts = requestLine.split(" ");
+
+                if (parts.length != 3) {
+                    continue;
+                }
+
+                final var path = parts[1];
+                if (!validPaths.contains(path)) {
+                    out.write((
+                            "HTTP/1.1 404 Not Found\r\n" +
+                                    "Content-Length: 0\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.flush();
+                    continue;
+                }
+
+                final var filePath = Path.of(".", "public", path);
+                final var mimeType = Files.probeContentType(filePath);
+
+                if (path.equals("/classic.html")) {
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    out.write(content);
+                    out.flush();
+                    continue;
+                }
+
+                final var length = Files.size(filePath);
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, out);
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
